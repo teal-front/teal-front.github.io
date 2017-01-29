641669d305cec9da9169e39127c5deabb57c7b12
teal-front <yaowang1@qq.com>
2017-01-29 16:59:22 +0800

    delete old files

--- WEB/JS_MODULES/RAW/RAW_OBJECT/Function.js
+++
@@ -1,4 +0,0 @@
- Function.prototype.bind = functionn () {
-	
- };
- 
--- WEB/JS_MODULES/RAW/getSelection.js
+++
@@ -1,5 +0,0 @@
-function getSelectedText() {
-	if(window.getSelection) {return window.getSelection().toString();}
-	else if(document.getSelection) {return document.getSelection();}
-	else if(document.selection) {return document.selection.createRange().text;}
-}
